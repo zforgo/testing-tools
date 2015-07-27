d25fc0153daaadd7b009ffd432a21190e082a8a1
zforgo <zforgo@gmail.com>
2015-07-27 18:20:10 +0200

    JUT-1 - Maven project and dummy classes just for the push

--- configurator/src/main/java/hu/zforgo/junit/tools/configuration/Dummy.java
+++
@@ -1,7 +0,0 @@
-package hu.zforgo.junit.tools.configuration;
-
-/**
- * Created by spinner on 7/27/15.
- */
-public class Dummy {
-}
--- context/src/main/java/hu/zforgo/junit/tools/context/Dummy.java
+++
@@ -1,7 +0,0 @@
-package hu.zforgo.junit.tools.context;
-
-/**
- * Created by spinner on 7/27/15.
- */
-public class Dummy {
-}
