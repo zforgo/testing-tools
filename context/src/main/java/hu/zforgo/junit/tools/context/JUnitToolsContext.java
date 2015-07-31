960e7c422457fa3baab83f6507b324a149e65c51
zforgo <zforgo@gmail.com>
2015-07-31 15:04:56 +0200

    JUT-2 Basic Context

---
+++ context/src/main/java/hu/zforgo/junit/tools/context/JUnitToolsContext.java
@@ -0,0 +1,41 @@
+package hu.zforgo.junit.tools.context;
+
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JUnitToolsContext {
+
+	private static final Logger LOG = LoggerFactory.getLogger(JUnitToolsContext.class);
+	private static volatile boolean inited;
+	private static volatile JUnitToolsContext instance;
+
+	private JUnitToolsContext() {
+	}
+
+	protected static synchronized void createContext(Description description) {
+		if (instance != null) {
+			LOG.warn("JUnitTools context class ({}) has already created", instance.getClass().getName());
+			return;
+		}
+		JUnitToolsContext currentContext = new JUnitToolsContext();
+		currentContext.init(description);
+
+		if (instance == null) {
+			instance = currentContext;
+		}
+	}
+
+	public static JUnitToolsContext getInstance() {
+		return instance;
+	}
+
+	protected void init(Description description) {
+		inited = true;
+	}
+
+	protected synchronized void finalize(Result result) {
+		inited = false;
+	}
+}
