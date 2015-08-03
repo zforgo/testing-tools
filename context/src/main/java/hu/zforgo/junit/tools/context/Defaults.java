39f42b742b6e6ef3561fa775d630072c229db57c
zforgo <zforgo@gmail.com>
2015-08-03 16:28:59 +0200

    JUT-4 Default config value holder

---
+++ context/src/main/java/hu/zforgo/junit/tools/context/Defaults.java
@@ -0,0 +1,9 @@
+package hu.zforgo.junit.tools.context;
+
+public interface Defaults {
+	String CONFIG_FILENAME = "junittools.properties";
+	String CONFIG_FILENAME_VARIABLE = "junittools.context.config";
+
+	String LOOKUP_ROOT = "config.root";
+
+}
