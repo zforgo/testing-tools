1f7f628e46dd8df2abbc58f1533f6cbcf63b2ddc
zforgo <zforgo@gmail.com>
2015-08-03 16:29:46 +0200

    JUT-10 load context configuration

---
+++ configurator/src/main/java/hu/zforgo/StringUtil.java
@@ -0,0 +1,18 @@
+package hu.zforgo;
+
+//TODO Refactor: move to the final module and package
+public class StringUtil {
+
+	public static final char DEFAULT_DELIMITER = ';';
+	public static final String DEFAULT_DELIMITER_STRING = String.valueOf(DEFAULT_DELIMITER);
+
+	public static final String[] EMPTY_STRING_ARRAY = new String[0];
+
+	public static boolean isEmpty(String input) {
+		return input == null || input.length() == 0;
+	}
+
+	public static boolean isNotEmpty(String input) {
+		return input != null && input.length() > 0;
+	}
+}
--- context/src/main/java/hu/zforgo/junit/tools/context/JUnitToolsContext.java
+++ context/src/main/java/hu/zforgo/junit/tools/context/JUnitToolsContext.java
@@ -1,20 +1,25 @@
 package hu.zforgo.junit.tools.context;
 
+import hu.zforgo.StringUtil;
+import hu.zforgo.junit.tools.configuration.Configuration;
+import hu.zforgo.junit.tools.configuration.ConfigurationFactory;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class JUnitToolsContext {
 
 	private static final Logger LOG = LoggerFactory.getLogger(JUnitToolsContext.class);
 	private static volatile boolean inited;
 	private static volatile JUnitToolsContext instance;
+	private static volatile ClassLoader contextClassLoader;
 
 	private JUnitToolsContext() {
 	}
 
-	protected static synchronized void createContext(Description description) {
+	protected static synchronized void createContext(Description description) throws ContextInitializationException {
 		if (instance != null) {
 			LOG.warn("JUnitTools context class ({}) has already created.", instance.getClass().getName());
 			return;
@@ -22,7 +27,28 @@ public class JUnitToolsContext {
 		LOG.info("Creating JUnitToolsContext...");
 		JUnitToolsContext currentContext = new JUnitToolsContext();
 
-		currentContext.init(description);
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		if (cl == null) {
+			cl = JUnitToolsContext_work.class.getClassLoader();
+			if (cl == null) {
+				cl = ClassLoader.getSystemClassLoader();
+			}
+		}
+		if (contextClassLoader == null) {
+			contextClassLoader = cl;
+		}
+
+		try {
+			Configuration contextConfig = ConfigurationFactory.load(StringUtil.isEmpty(System.getProperty(Defaults.CONFIG_FILENAME_VARIABLE)) ? Defaults.CONFIG_FILENAME : System.getProperty(Defaults.CONFIG_FILENAME_VARIABLE));
+
+			currentContext.init(description);
+		} catch (ContextInitializationException e) {
+			LOG.error("An error occurred during initialize context", e);
+
+		} catch (Exception e) {
+			LOG.error("A fatal error occurred during initialize context", e);
+			throw new ContextInitializationFailure(e);
+		}
 		LOG.info("JUnitToolsContext was successfully initialized.");
 
 		if (instance == null) {
@@ -34,7 +60,7 @@ public class JUnitToolsContext {
 		return instance;
 	}
 
-	protected void init(Description description) {
+	protected void init(Description description) throws ContextInitializationException {
 		inited = true;
 	}
 
