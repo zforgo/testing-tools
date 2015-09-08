208c267f9bea09d296838ba512e01a6de4ece0f5
zforgo <zforgo@gmail.com>
2015-09-08 20:26:04 +0200

    JUT-18 multi module pathprovider integration test

---
+++ context/src/it/002-multiple-pathprovider/module-a/src/main/java/hu/zforgo/junit/tools/context/ModuleAConfigPathProvider.java
@@ -0,0 +1,16 @@
+package hu.zforgo.junit.tools.context;
+
+import hu.zforgo.junit.tools.configuration.Configuration;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ModuleAConfigPathProvider implements ConfigurationPathProvider {
+
+	@Override
+	public List<Path> getPaths(final Path root, Configuration props) {
+		return Stream.of("moduleA/foo", "moduleA/foo/bar", "moduleA/foo/bar/baz").map(root::resolve).collect(Collectors.toList());
+	}
+}
