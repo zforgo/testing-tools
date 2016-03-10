3941013a611c3ba8bbacf0b569bda426f34bb18d
zforgo <zforgo@gmail.com>
2016-03-10 11:18:21 +0100

    TNT-52 Proxy configuration integration tests

--- selenium-tools/selenium-commons/src/it/001-context/src/test/java/hu/zforgo/ProxyTest.java
+++ selenium-tools/selenium-commons/src/it/001-context/src/test/java/hu/zforgo/ProxyTest.java
@@ -3,6 +3,8 @@ package hu.zforgo;
 import hu.zforgo.common.util.ClassUtil;
 import hu.zforgo.testing.selenium.DriverSetup;
 import hu.zforgo.testing.selenium.SeleniumContext;
+import hu.zforgo.testing.tools.configuration.Configuration;
+import javaslang.Tuple2;
 import org.junit.Test;
 import org.openqa.selenium.Proxy;
 
@@ -14,26 +16,25 @@ public class ProxyTest {
 
 	@Test
 	public void proxyTest() throws NoSuchFieldException {
-		Map<DriverSetup, Proxy> proxies = ClassUtil.fieldValue(SeleniumContext.class, "configuredProxies");
+		Map<DriverSetup, Tuple2<Proxy, Configuration>> config = ClassUtil.fieldValue(SeleniumContext.class, "driverConfig");
 		{
-			Proxy p = proxies.get(DriverSetup.CHROME);
+			Proxy p = config.get(DriverSetup.CHROME)._1;
 			assertThat(p).isNull();
 		}
 		{
-			Proxy p = proxies.get(DriverSetup.FIREFOX);
+			Proxy p = config.get(DriverSetup.FIREFOX)._1;
 
 			assertThat(p.isAutodetect()).isTrue();
 			assertThat(p.getFtpProxy()).isNullOrEmpty();
 			assertThat(p.getHttpProxy()).isNullOrEmpty();
 			assertThat(p.getProxyType()).isEqualTo(Proxy.ProxyType.AUTODETECT);
 		}
-//		{
-//			Proxy p = proxies.get(DriverSetup.IE);
-//
-//			assertThat(p.isAutodetect()).isFalse();
-//			assertThat(p.getFtpProxy()).isEqualTo("localhost:9321");
-//			assertThat(p.getHttpProxy()).isNullOrEmpty();
-//			assertThat(p.getProxyType()).isEqualTo(Proxy.ProxyType.MANUAL);
-//		}
+		{
+			Proxy p = config.get(DriverSetup.HTMLUNIT)._1;
+			assertThat(p.isAutodetect()).isFalse();
+			assertThat(p.getFtpProxy()).isEqualTo("localhost:9321");
+			assertThat(p.getHttpProxy()).isNullOrEmpty();
+			assertThat(p.getProxyType()).isEqualTo(Proxy.ProxyType.MANUAL);
+		}
 	}
 }
