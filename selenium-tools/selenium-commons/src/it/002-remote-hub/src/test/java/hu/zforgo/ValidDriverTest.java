b53904a7eb55189b594c4356aecae331bf79bbfc
zforgo <zforgo@gmail.com>
2016-11-06 20:28:16 +0100

    Upgrade Selenium version and introduce marionette driver support

--- selenium-tools/selenium-commons/src/it/002-remote-hub/src/test/java/hu/zforgo/ValidDriverTest.java
+++ selenium-tools/selenium-commons/src/it/002-remote-hub/src/test/java/hu/zforgo/ValidDriverTest.java
@@ -1,5 +1,6 @@
 package hu.zforgo;
 
+import com.google.common.base.Predicate;
 import hu.zforgo.testing.selenium.DriverSetup;
 import hu.zforgo.testing.selenium.SeleniumContext;
 import org.junit.After;
@@ -9,6 +10,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Collection;
 import java.util.stream.Collectors;
@@ -67,6 +69,13 @@ public class ValidDriverTest {
 	public void startPageTest() {
 		Assume.assumeFalse(String.format("The driver [%s] doesn't support startPage option. Ignored.", driverSetup.name()), driverSetup == DriverSetup.HTMLUNIT);
 		// startpage check
+		WebDriverWait w = new WebDriverWait(driver,2_000L);
+		w.until(new Predicate<WebDriver>() {
+			@Override
+			public boolean apply(WebDriver input) {
+				return input.getCurrentUrl().equals(START_PAGE);
+			}
+		});
 		assertThat(driver.getCurrentUrl()).as("Check Startup page for driver: %s", driverSetup.name()).isEqualTo(START_PAGE);
 	}
 
